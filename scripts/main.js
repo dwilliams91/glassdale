cf33979f6de14107f224f8b49b7b0d65a8a1fbc1
Your Github User Name <david.lav.williams@gmail.com>
2020-10-15 15:19:37 -0500

    imported in criminals

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,7 @@
+import { getOfficers } from './officers/OfficerProvider.js'
+import {getCriminals} from './criminals/CriminalProvider.js'
+
 console.log("Welcome to the main module")
+ 
+console.log(getOfficers())
+console.log(getCriminals())
