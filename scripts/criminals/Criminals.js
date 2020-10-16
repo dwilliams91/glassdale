b6f85819048329259c30fccb310ce06de50f9018
Your Github User Name <david.lav.williams@gmail.com>
2020-10-16 11:38:30 -0500

    the date is now displaying right

--- scripts/criminals/Criminals.js
+++ scripts/criminals/Criminals.js
@@ -1,11 +1,11 @@
 export const createCriminalCard=(criminalObj)=>{
     return `
     <div class=criminalCard>
-    <h5>${criminalObj.name}</h5>
+    <h4>${criminalObj.name}</h4>
     <p>Age: ${criminalObj.age}</p>
     <p>Crime: ${criminalObj.conviction}</p>
-    <p>Term Start: ${criminalObj.incarceration.start}</p>
-    <p>Term End: ${criminalObj.incarceration.end}</p>
+    <p>Term Start: ${new Date(criminalObj.incarceration.start).toLocaleDateString('en-US')}</p>
+    <p>Term End: ${new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')}</p>
     </div>
 
     `
