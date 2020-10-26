f27792d8568fe4184ee914df28ea4dfd8286c19f
Your Github User Name <david.lav.williams@gmail.com>
2020-10-26 11:18:04 -0500

    added some notes walking through the alibi list

--- scripts/criminals/AlibiList.js
+++ scripts/criminals/AlibiList.js
@@ -2,16 +2,16 @@
 import { useCriminals } from "./CriminalProvider.js"
 const eventHub = document.querySelector(".container")
 
+// creating a function with an eventlistener that is being exported to the dom
 export const CreatAlibiEventListener=()=>{
-
-    
-    
     eventHub.addEventListener("alibiButtonClicked", (eventObj)=>{
+        // getting the criminals
         const arrayOfCriminals=useCriminals()
+        // returning the criminal who have the id that is the same as the id of the button clicked. 
         const foundCriminal=arrayOfCriminals.find((criminalObj)=> {
             return criminalObj.id===parseInt(eventObj.detail.criminalId)
         })
-    
+        // doing the alibiList function for criminal found
         AlibiList(foundCriminal)
     })
 
@@ -27,7 +27,9 @@ const AlibiList=(criminalObj)=>{
 
 
 const render=(criminalObj)=>{
+    // finds the div with the id of the criminal of the found criminal
     const contentTarget=document.querySelector(`#criminal-${criminalObj.id}`) 
+    // adds html to it and goes through each of the known associates and returns the name and alibi
     contentTarget.innerHTML+=
     `
     <div class=alibi__list>
