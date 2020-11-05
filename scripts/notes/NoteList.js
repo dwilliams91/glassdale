5c05ea24e40e360de1e0e9457589b115d7bf42c7
Your Github User Name <david.lav.williams@gmail.com>
2020-11-05 09:07:09 -0600

    its not working yet

--- scripts/notes/NoteList.js
+++ scripts/notes/NoteList.js
@@ -1,6 +1,7 @@
 
 const eventHub=document.querySelector(".container")
 const contentTarget=document.querySelector(".notesContainer")
+import { getCriminals, useCriminals } from '../criminals/CriminalProvider.js'
 import{createNoteCard} from './NoteHTMLConverter.js'
 import { useNotes, getNotes} from './NoteProvider.js'
 
@@ -10,19 +11,54 @@ eventHub.addEventListener("noteStateChanged", ()=>noteList())
 
 export const noteList=()=>{
     getNotes()
+    .then(getCriminals)
     .then(()=>{
+        const allCriminals=useCriminals()
         const allNotes=useNotes()
-        render(allNotes)
+        render(allNotes, allCriminals)
     })
 }
 
-const render = noteArray => {
-
-    let noteHTML=""
-    for (const note of noteArray){
-        noteHTML +=createNoteCard(note)
-    }
-    // console.log(noteHTML)
-    contentTarget.innerHTML = `${noteHTML}`
+const render = (noteCollection, criminalCollection) => {
+    contentTarget.innerHTML = noteCollection.map(note => {
+        console.log(note)
+        // Find the related criminal
+        const relatedCriminal = criminalCollection.find(criminal => criminal.id === note.criminalId)
+        console.log(relatedCriminal)
+        return `
+            <section class="note">
+                <h2>Note about ${relatedCriminal.name}</h2>
+                ${note.noteText}
+            </section>
+        `
+    })
 }
 
+// import { useNotes } from './NoteProvider.js'
+// import { useCriminals } from '../criminals/CriminalProvider.js'
+
+
+// const render = (noteCollection, criminalCollection) => {
+//     contentTarget.innerHTML = noteCollection.map(note => {
+//         // Find the related criminal
+//         const relatedCriminal = criminalCollection.find(criminal => criminal.id === note.criminalId)
+
+//         return `
+//             <section class="note">
+//                 <h2>Note about ${relatedCriminal.name}</h2>
+//                 ${note.noteText}
+//             </section>
+//         `
+//     })
+// }
+
+// const NoteList = () => {
+//     getNotes()
+//         .then(getCriminals)
+//         .then(() => {
+//             const notes = useNotes()
+//             const criminals = useCriminals()
+
+//             render(notes, criminals)
+//         })
+// }
