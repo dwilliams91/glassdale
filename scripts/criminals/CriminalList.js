7bafbc7d9cea5dd742fad399271dd6eccb4a6afb
Your Github User Name <david.lav.williams@gmail.com>
2020-11-09 14:48:24 -0600

    the facilities are still broken

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -1,11 +1,10 @@
 import { useConvictions } from "../convictions/ConvictionProvider.js"
 import { useOfficers } from "../officers/OfficerProvider.js"
 import { useCriminals, getCriminals } from "./CriminalProvider.js"
-import { createCriminalCard } from "./Criminals.js"
-
-
+import { Criminal } from "./Criminals.js"
+import {getCriminalFacilities, useCriminalFacilities} from '../facilities/CriminalFacilityProvider.js'
+import {getFacilities, useFacilities} from '../facilities/FacilityProvider.js'
 const eventHub = document.querySelector(".container")
-
 
 // dispatching the alibi event listener
 
@@ -40,26 +39,58 @@ eventHub.addEventListener("officerSelected", event => {
         
     }
 })
+const render = (criminalsToRender, allFacilities, allRelationships) => {
+    // Step 1 - Iterate all criminals
+    const contentTarget=document.querySelector(".criminalsContainer")
 
+    console.log(contentTarget)
+    contentTarget.innerHTML = criminalsToRender.map(
+        (criminalObject) => {
+            // Step 2 - Filter all relationships to get only ones for this criminal
+            const facilityRelationshipsForThisCriminal = allRelationships.filter(cf => cf.criminalId === criminalObject.id)
+
+            // Step 3 - Convert the relationships to facilities with map()
+            const facilities = facilityRelationshipsForThisCriminal.map(cf => {
+                const matchingFacilityObject = allFacilities.find(facility => facility.id === cf.facilityId)
+                return matchingFacilityObject
+            })
+
+            // Must pass the matching facilities to the Criminal component
+            return Criminal(criminalObject, facilities)
+        }
+    ).join("")
+}
 
 // calling criminal.js for each individual criminal
-const render = criminalCollection => {
-    const contentTarget = document.querySelector(".criminalsContainer")
-    let crinimalHTML=""
-    for (const crime of criminalCollection){
-        crinimalHTML +=createCriminalCard(crime)
-    }
-    contentTarget.innerHTML = crinimalHTML
-}
+// const render = criminalCollection => {
+//     const contentTarget = document.querySelector(".criminalsContainer")
+//     let crinimalHTML=""
+//     for (const crime of criminalCollection){
+//         crinimalHTML +=createCriminalCard(crime)
+//     }
+//     contentTarget.innerHTML = crinimalHTML
+// }
 
 
 // Render ALL criminals initally
 export const CriminalList = () => {
     getCriminals()
-        .then(() => {
-            const appStateCriminals = useCriminals()
-            render(appStateCriminals)
-        })
+    .then(getCriminalFacilities())
+    .then(getFacilities())
+        .then(getCriminalFacilities)
+        .then(
+            () => {
+                // Pull in the data now that it has been fetched
+                const facilities = useFacilities()
+                const crimFac = useCriminalFacilities()
+                const criminals = useCriminals()
+
+                // Pass all three collections of data to render()
+                render(criminals, facilities, crimFac)
+            }
+        )
+
+    
 }
 
 // end of my stuff
